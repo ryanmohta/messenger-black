e62b3a10ae33bd8460d07b17b6cd84b7bc74913d
Ryan Mohta <ryanmohta@gmail.com>
2018-11-01 00:51:44 -0700

    Trying stuff out

--- Messenger Black for Safari/script.js
+++ Messenger Black for Safari/script.js
@@ -1,16 +1,24 @@
 var body = document.getElementsByTagName("BODY")[0];
 
 document.addEventListener("DOMContentLoaded", function(event) {
-                          safari.extension.dispatchMessage("Hello World!");
+                          var date = new Date();
+                          var hours = date.getHours();
+                          if(hours < 7 || hours > 18) {
+                            document.getElementsByTagName("BODY")[0].classList.add("blackMode");
+                          }
+                          else {
+                            document.getElementsByTagName("BODY")[0].classList.remove("blackMode");
+                          }
+                          //safari.extension.dispatchMessage("Hello World!");
 });
 
-safari.self.addEventListener("message", handleMessage);
+//safari.self.addEventListener("message", handleMessage);
 
-function handleMessage(event) {
+/*function handleMessage(event) {
     if(event.name == "ExtensionOff") {
         document.getElementsByTagName("BODY")[0].classList.remove("blackMode");
     }
     else {
         document.getElementsByTagName("BODY")[0].classList.add("blackMode");
     }
-}
+}*/
