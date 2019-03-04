b363ac53d3c03f8c2adbd92831b6dfaaee1db1e7
Ryan Mohta <ryanmohta@gmail.com>
2019-03-04 01:47:05 -0800

    Updated popup

--- Messenger Black for Safari/SafariExtensionViewController.swift
+++ Messenger Black for Safari/SafariExtensionViewController.swift
@@ -15,5 +15,29 @@ class SafariExtensionViewController: SFSafariExtensionViewController {
         shared.preferredContentSize = NSSize(width:240, height:240)
         return shared
     }()
-
+    
+    @IBOutlet weak var manuallyEnable: NSButton!
+    
+    @IBOutlet weak var scheduled: NSButton!
+    
+    @IBOutlet weak var sunsetToSunrise: NSButton!
+    
+    @IBOutlet weak var startTime: NSDatePicker!
+    
+    @IBOutlet weak var endTime: NSDatePicker!
+    
+    
+    
+    @IBAction func radioButton(_ sender: Any) {
+        if(scheduled.state == NSControl.StateValue.on) {
+            startTime.isEnabled = true;
+            endTime.isEnabled = true;
+        }
+        else {
+            startTime.isEnabled = false;
+            endTime.isEnabled = false;
+        }
+        
+    }
+    
 }
