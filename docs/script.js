eb25b579861361b6636f25c525405357d276e7f3
Ryan Mohta <ryanmohta@gmail.com>
2019-08-27 20:04:22 -0700

    Added javascript

---
+++ docs/script.js
@@ -0,0 +1,19 @@
+document.body.onscroll = function() {
+  var title = document.getElementById("title")
+  var rect = title.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+
+  if (elemTop < 0) {
+    document.getElementById("navbar").visibility = visible;
+  }
+  else {
+    document.getElementById("navbar").visibility = hidden;
+  }
+
+    // // Only completely visible elements return true:
+    // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // // Partially visible elements return true:
+    // //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    // return isVisible;
+}
