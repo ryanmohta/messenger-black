af6a136a4142cdd87e79b40568a5dfb2f159b7c3
Ryan Mohta <ryanmohta@gmail.com>
2019-08-27 20:07:26 -0700

    js

--- docs/script.js
+++ docs/script.js
@@ -1,19 +1,22 @@
-document.body.onscroll = function() {
-  var title = document.getElementById("title")
-  var rect = title.getBoundingClientRect();
-  var elemTop = rect.top;
-  var elemBottom = rect.bottom;
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.onscroll = function() {
+    var title = document.getElementById("title")
+    var rect = title.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
 
-  if (elemTop < 0) {
-    document.getElementById("navbar").visibility = visible;
-  }
-  else {
-    document.getElementById("navbar").visibility = hidden;
-  }
+    if (elemTop < 0) {
+      document.getElementById("navbar").visibility = visible;
+    }
+    else {
+      document.getElementById("navbar").visibility = hidden;
+    }
 
-    // // Only completely visible elements return true:
-    // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // // Partially visible elements return true:
-    // //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    // return isVisible;
-}
+      // // Only completely visible elements return true:
+      // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+      // // Partially visible elements return true:
+      // //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+      // return isVisible;
+  };
+
+});
