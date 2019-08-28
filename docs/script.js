b271c72938ac49ff8c61a4ccf6859d2badf05546
Ryan Mohta <ryanmohta@gmail.com>
2019-08-27 22:25:29 -0700

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -16,6 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 });
 
-// document.addEventListener("load", function() {
-//   document.getElementById("title").classList.add("loaded");
-// });
+window.addEventListener("load", function() {
+  document.getElementById("title").classList.add("loaded");
+});
